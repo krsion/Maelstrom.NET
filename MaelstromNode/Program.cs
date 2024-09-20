ab79a1d5753ad771592113755194b0c092250af9
Alex Lodge <awlodge@gmail.com>
2024-09-20 10:31:32 +0100

    Make it  a worker project

--- MaelstromNode/Program.cs
+++ MaelstromNode/Program.cs
@@ -1,22 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MaelstromNode;
 using MaelstromNode.Interfaces;
 using MaelstromNode.Workloads;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace MaelstromNode;
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<EchoServer>();
+builder.Services.AddSingleton<IReceiver, StdinReceiver>();
+builder.Services.AddSingleton<ISender, StdoutSender>();
 
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-        //builder.Logging.AddConsole();
-        builder.Services.AddHostedService<EchoServer>();
-        builder.Services.AddSingleton<IReceiver, StdinReceiver>();
-        builder.Services.AddSingleton<ISender, StdoutSender>();
-
-        using IHost host = builder.Build();
-        await host.RunAsync();
-    }
-}
+using IHost host = builder.Build();
+await host.RunAsync();
