6858bf78c80d9a20a8489852b2edf71743c4c72e
Alex Lodge <awlodge@gmail.com>
2024-10-02 15:40:44 +0100

    Normalise namespaces

--- Maelstrom/StdoutSender.cs
+++ Maelstrom/StdoutSender.cs
@@ -1,24 +1,23 @@
 ﻿using Maelstrom.Interfaces;
 
-namespace Maelstrom
-{
-    internal class StdoutSender : ISender
-    {
-        private readonly StreamWriter _stream;
-        public StdoutSender()
-        {
-            var outputStream = Console.OpenStandardOutput();
-            _stream = new StreamWriter(outputStream)
-            {
-                AutoFlush = true
-            };
-            Console.SetOut(_stream);
-        }
-        public async Task SendAsync(string message)
-        {
-            await _stream.WriteLineAsync(message);
-        }
+namespace Maelstrom;
 
-        public void Dispose() => _stream.Dispose();
+internal class StdoutSender : ISender
+{
+    private readonly StreamWriter _stream;
+    public StdoutSender()
+    {
+        var outputStream = Console.OpenStandardOutput();
+        _stream = new StreamWriter(outputStream)
+        {
+            AutoFlush = true
+        };
+        Console.SetOut(_stream);
     }
+    public async Task SendAsync(string message)
+    {
+        await _stream.WriteLineAsync(message);
+    }
+
+    public void Dispose() => _stream.Dispose();
 }
